c37dd1156db10290e3129b36701621436e3ce836
Pauric Holleran <pauric.holleran@gmail.com>
2017-01-09 07:23:43 +0000

    /new/ route added

--- app.js
+++ app.js
@@ -3,26 +3,33 @@ const app = express();
 const url = require('url');
 const path = require('path');
 const validUrl = require('valid-url');
+const shortid = require('shortid');
 var userURL; // URL submitted by user
+var shortURL; // Short URL generated
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', function(req, res) {
-        res.sendfile(path.join(__dirname + '/public/index.html'));
+  res.sendfile(path.join(__dirname + '/public/index.html'));
 });
 
 app.get('/*', function(req, res) {
   userURL = url.parse(req.url, true).path.slice(1);
-  if (validUrl.isUri(userURL)){
-        res.send('Looks like an URI');
-    } else {
-        res.send('Not a URI');
+  console.log(userURL);
+  if (userURL.slice(0, 3) === '/new') {
+    if (validUrl.isUri(userURL)) {
+      shortURL = shortid.generate();
+      res.send('Looks like an URI ' + '\n' +
+        'Visit ' + shortURL + 'to be redirected');
     }
+    else {
+      res.send('Not a URI');
+    }
+  }
+  else if (userURL === shortURL) {
+    res.redirect(userURL);
+  }
 });
-  
-
 
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("URL shortener listening on port " + process.env.PORT);
