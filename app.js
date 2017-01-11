09a03b633c17b96ab5463c538f72741d6d93c33b
Pauric Holleran <pauric.holleran@gmail.com>
2017-01-11 22:01:28 +0000

    mongodb added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shortid = require('shortid');
 var userURL; // URL submitted by user
 var shortURL; // Short URL generated
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+//app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.sendfile(path.join(__dirname + '/public/index.html'));
@@ -15,20 +15,30 @@ app.get('/', function(req, res) {
 
 app.get('/*', function(req, res) {
   userURL = url.parse(req.url, true).path.slice(1);
-  console.log(userURL);
-  if (userURL.slice(0, 3) === '/new') {
+  console.log("URL is: " + userURL);
+  if (userURL.slice(0, 4) === 'new/') {
+    console.log("after conditional: " + userURL);
+    userURL = userURL.slice(4);
+    console.log("after slice: " + userURL);
     if (validUrl.isUri(userURL)) {
       shortURL = shortid.generate();
+      console.log(shortURL);
       res.send('Looks like an URI ' + '\n' +
-        'Visit ' + shortURL + 'to be redirected');
+        'Visit ' + shortURL + ' to be redirected');
     }
     else {
+      console.log('not valid');
       res.send('Not a URI');
     }
   }
   else if (userURL === shortURL) {
-    res.redirect(userURL);
+    //res.redirect(userURL);
+    res.send('Retrieve URL to send. No longer have it though!');
   }
+  else {
+    console.log('no conditionals true');
+  }
+  
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
